667524a5291c567ac7f5e4cd6b276a7d050c5b50
Graham Reeves <graham@grahamreeves.com>
2021-07-30 19:45:06 +0100

    Now parsing popcap files

--- PopCapToRainbow.js/App.js
+++ PopCapToRainbow.js/App.js
@@ -1,92 +1,69 @@
 export default 'App.js';
 import Pop from './PopEngineCommon/PopEngine.js'
+import PopCapDecoder from './PopCap.js'
 
 let TableGui = null;
-let Mp4Sections = [];
+let Sections = [];
 
-function PushMp4Atoms(Atoms)
+function PushFrames(Frames)
 {
-	function ToRow(Atom)
+	function ToRow(Frame)
 	{
 		const Row = {};
-		Row.Fourcc = Atom.Fourcc;
-		Row.FilePosition = Atom.FilePosition;
-		Row.HeaderSize = Atom.HeaderSize;
-		Row.ContentSize = Atom.ContentSize;
+		Row.Stream = Frame.Meta.Stream;
+		//Row.CameraName = Frame.Meta.CameraName;
+		Row.OutputTimeMs = Frame.Meta.OutputTimeMs;
+		Row.Keyframe = Frame.Meta.Keyframe;
+		Row.WidthHeight = `${Frame.Meta.Width}x${Frame.Meta.Height}`;
+		//Row.Meta = Frame.Meta;
+		Row.DataSize = Frame.Data.length;
 		return Row;
 	}
 
-	const Rows = Atoms.map(ToRow);
-	Mp4Sections.push(...Rows);
-	TableGui.SetValue(Mp4Sections);
-}
-
-function PushMp4Samples(Samples)
-{
-	function ToRow(Sample)
-	{
-		const Row = {};
-		Row.DecodeTimeMs = Sample.DecodeTimeMs;
-		Row.PresentationTimeMs = Sample.PresentationTimeMs;
-		Row.ContentSize = Sample.DataSize;
-		Row.Keyframe = Sample.IsKeyframe ? 'Keyframe' : '';
-		Row.FilePosition = Sample.FilePosition || Sample.DataFilePosition || Sample.DataPosition;
-		Row.Flags = `0x` + Sample.Flags.toString(16);
-		return Row;
-	}
-
-	const Rows = Samples.map(ToRow);
-	Mp4Sections.push(...Rows);
-	TableGui.SetValue(Mp4Sections);
-}
-
-function ClearMp4Sections()
-{
-	Mp4Sections = [];
+	const Rows = Frames.map(ToRow);
+	Sections.push(...Rows);
+	TableGui.SetValue(Sections);
 }
 
 
-export async function LoadMp4(Filename)
+function ClearSections()
 {
-	const Mp4 = new Mp4Decoder();
-	ClearMp4Sections();
+	Sections = [];
+}
+
+
+export async function LoadPopCap(Filename)
+{
+	const Decoder = new PopCapDecoder();
+	ClearSections();
 	
 	//	async callback for new data
-	async function ReadDecodedAtomThread()
+	async function ReadDecodedFrameThread()
 	{
 		while ( true )
 		{
-			const Atom = await Mp4.WaitForNextAtom();
-			PushMp4Atoms( [Atom] );
+			const Frame = await Decoder.WaitForNextFrame();
+			PushFrames( [Frame] );
 			//	detect EOF
 		}
 	}
-	async function ReadDecodedSampleThread()
-	{
-		while ( true )
-		{
-			const Samples = await Mp4.WaitForNextSamples();
-			PushMp4Samples( Samples );
-		}
-	}
-	const DecodeAtomThreadPromise = ReadDecodedAtomThread();
-	const DecodeSampleThreadPromise = ReadDecodedSampleThread();
+	const DecodeFrameThreadPromise = ReadDecodedFrameThread();
 	
 	//	async loading & feeding data
 	async function ReadFileThread()
 	{
 		function OnNewChunk(Contents)
 		{
-			Mp4.PushData(Contents);
+			Decoder.PushData(Contents);
 		}
 		const ResolveChunks = false;
 		const FilePromise = Pop.FileSystem.LoadFileAsArrayBufferStreamAsync(Filename,ResolveChunks,OnNewChunk);
 		await FilePromise;
-		Mp4.PushEndOfFile();
+		Decoder.PushEndOfFile();
 	}
 	const ReadFilePromise = ReadFileThread();
 	
-	const WaitAllResult = await Promise.all( [DecodeAtomThreadPromise,DecodeSampleThreadPromise,ReadFilePromise] );
+	const WaitAllResult = await Promise.all( [DecodeFrameThreadPromise,ReadFilePromise] );
 	Pop.Debug(`File loaded; ${WaitAllResult}`,WaitAllResult);
 }
 
@@ -102,6 +79,6 @@ async function DragAndDropThread(DropTargetElement)
 	{
 		const DroppedFilename = await DropTargetElement.WaitForDragDrop();
 		Pop.Debug(`Dropped File; ${DroppedFilename}`);
-		await LoadMp4(DroppedFilename);
+		await LoadPopCap(DroppedFilename);
 	}
 }
--- PopCapToRainbow.js/PopCap.js
+++ PopCapToRainbow.js/PopCap.js
@@ -1,13 +1,83 @@
 //	gr: I did make an effecient decoder, but its so simple and not important here, Im just gonna write another
+import Pop from './PopEngineCommon/PopEngine.js'
+import PromiseQueue from './PopEngineCommon/PromiseQueue.js'
+import {DataReader,EndOfFileMarker} from './PopEngineCommon/DataReader.js'
+import {StringToBytes,BytesToString} from './PopEngineCommon/PopApi.js'
 
+const PacketMarker = StringToBytes('Pop\n');
+
+//	convert to string, but also validate is json
+function BytesToJson(Bytes)
+{
+	const String = BytesToString(Bytes);
+	const Json = JSON.parse(String);
+	return Json;
+}
 
 export default class PopCapDecoder
 {
 	constructor()
 	{
+		this.NewFrameQueue = new PromiseQueue('PopCap decoder frame queue');
+	
+		this.NewByteQueue = new PromiseQueue('PopCap pending bytes');
+		this.FileReader = new DataReader( new Uint8Array(0), 0, this.WaitForMoreFileData.bind(this) );
+		
+		this.ParsePromise = this.ParseFileThread();
+	}
+	
+	async WaitForMoreFileData()
+	{
+		return this.NewByteQueue.WaitForNext();
 	}
 	
 	async WaitForNextFrame()
 	{
+		return this.NewFrameQueue.WaitForNext();
 	}
 
+	PushEndOfFile()
+	{
+		this.PushData(EndOfFileMarker);
+	}
+	
+	PushData(Bytes)
+	{
+		this.NewByteQueue.Push(Bytes);
+	}
+
+	OnFrame(Frame)
+	{
+		//	todo: push into a list for different streams, then output synchronised stream packets
+		this.NewFrameQueue.Push(Frame);
+	}
+	
+	async ParseFileThread()
+	{
+		while ( true )
+		{
+			await Pop.Yield(0);
+			
+			const NextMetaPacket = await this.FileReader.ReadUntilMatch(PacketMarker,false);
+			//	probably a marker at the start of a file (or Pop\nPop\n somehwere)
+			if ( NextMetaPacket.length == 0 )
+				continue;
+
+			const Frame = {};
+			try
+			{
+				Frame.Meta = BytesToJson(NextMetaPacket);
+			}
+			catch(e)
+			{
+				Pop.Warning(`Next packet didn't turn to meta as expected`);
+				continue;
+			}
+			
+			//	bytes next
+			Frame.Data = await this.FileReader.ReadUntilMatch(PacketMarker,false);
+			
+			this.OnFrame(Frame);
+		}
+	}
+}
